38249d7694574792c2e91d13bc7e1495a262557a
Genius0412 <genius0412.tech@gmail.com>
2020-12-20 19:38:27 +0900

    commit

--- beta 2.1/index.js
+++ beta 2.1/index.js
@@ -1,5 +1,6 @@
 var con = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 var vow = ["a", "e", "i", "o", "u"]
+var alp = con.concat(vow)
 var phr = ["ext", "jit", "ker", "ex", "re", "xe"]
 
 const rand = (max) => {
@@ -27,11 +28,17 @@ const gen = () => {
     const len = parseInt(prompt('아이디의 글자수 입력 (3 이상)'))
     var id = ""
     
+
+    //check if the length is less than 3
     if(len < 3) console.error('Error: length should be larger than 3');
-    var temp = rand(phr.length+7)
+
+    //add a phrase at the start of the id
+    var temp = rand(phr.length*2)
     if(temp <= (phr.length-1)) id += phr[temp]
-    for(var i = id.length+1; i <= len; i++){
-        if(charType(id[i-2]) == "consonant") id += vow[rand(vow.length-1)]
+    else id += alp[rand(alp.length-1)]
+
+    for(var i = id.length; i < len; i++){
+        if(charType(id[i-1]) == "consonant") id += vow[rand(vow.length-1)]
         else id += con[rand(con.length-1)]
     }
     alert(`아이디가 완성되었습니다\n${id}`)
